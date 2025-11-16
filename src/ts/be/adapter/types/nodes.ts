7939457b10e7b923f3c3eca33a67a5750b602665
AlexDev404 <34188635+AlexDev404@users.noreply.github.com>
2025-11-16 14:14:44 -0600

    patch: Update Node type to accurately reflect current API changes

--- src/ts/be/adapter/types/nodes.ts
+++ src/ts/be/adapter/types/nodes.ts
@@ -6,15 +6,18 @@ export type NodeStatus = 'ONLINE' | 'OFFLINE' | 'ERROR';
 
 export interface NodeCreateRequest {
 	status: NodeStatus;
+	status_details?: string;
 }
 
 export interface NodeUpdateRequest {
 	status?: NodeStatus;
+	status_details?: string;
 }
 
 export interface NodeResponse {
 	device_id: number;
 	status: NodeStatus;
+	status_details?: string;
 }
 
 export interface NodesResponse {
